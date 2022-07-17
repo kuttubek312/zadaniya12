21f5c194ba7c411dfa3d49c66a5334b5012b94b8
kuttubek <zhalalov98@internet.ru>
2022-07-17 10:55:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.a = 13;
+        triangle.b = 14;
+        triangle.c = 15;
+        triangle.area();
+
+    }
+
+
+}
+
+
+
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Triangle {
+    int a = 13;
+    int b = 14;
+    int c = 15;
+
+
+
+    void area() {
+        double p = (13 + 14 + 15)/2;
+        double ar = Math.sqrt(21 * 8 * 7 * 6);
+        System.out.println(ar);
+    }
+}
+
